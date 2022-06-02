2f9244d1a015f4196eb91abb5e1fdb2a22025233
KC Hofstetter <kclynhofstetter@gmail.com>
2022-06-02 15:06:07 -0700

    finished up the CSS

--- js/compatibility.js
+++ js/compatibility.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let userHoroscopes;
-let compatibilityMain = document.getElementById('compatibilityMain');
+let matchList = document.getElementById('matchList');
 let userNameComp = document.getElementById('userNameComp');
 let userDayComp = document.getElementById('userDayComp');
 let userMonthComp = document.getElementById('userMonthComp');
@@ -34,30 +34,44 @@ for (let i = 0; i < userHoroscopes.length; i++) {
   if (currentHoroscopeComp.compatibility === userHoroscopes[i].compatibility){
     matchFound = true;
     let article = document.createElement('article');
-    compatibilityMain.appendChild(article);
+    article.className = 'sectionJS';
+    matchList.appendChild(article);
+
+    let h3 = document.createElement('h3');
+    h3.textContent = 'You\'ve got a match!';
+    h3.className = 'h3JS';
+    article.appendChild(h3);
 
     let image = document.createElement('img');
+    image.className = 'imgJS';
     article.appendChild(image);
 
     let list = document.createElement('ul');
+    list.className = 'ulJS'
     article.appendChild(list);
 
     let listItem1 = document.createElement('li');
+    listItem1.className = 'infoJS';
     list.appendChild(listItem1);
 
     let listItem2 = document.createElement('li');
+    listItem2.className = 'infoJS';
     list.appendChild(listItem2);
 
     let listItem3 = document.createElement('li');
+    listItem3.className = 'infoJS';
     list.appendChild(listItem3);
 
     let listItem4 = document.createElement('li');
+    listItem4.className = 'infoJS';
     list.appendChild(listItem4);
 
     let listItem5 = document.createElement('li');
+    listItem5.className = 'infoJS';
     list.appendChild(listItem5);
 
     let listItem6 = document.createElement('li');
+    listItem6.className = 'infoJS';
     list.appendChild(listItem6);
 
     image.src = userHoroscopes[i].image;
--- js/horoscopes.js
+++ js/horoscopes.js
@@ -14,30 +14,39 @@ if (localStorageData === null) {
 for (let i = 0; i < userHoroscopes.length; i++) {
 
   let article = document.createElement('article');
+  article.className = 'articleJS';
   main.appendChild(article);
 
   let image = document.createElement('img');
+  image.className = 'imgJS';
   article.appendChild(image);
 
   let list = document.createElement('ul');
+  list.className = 'ulJS';
   article.appendChild(list);
 
   let listItem1 = document.createElement('li');
+  listItem1.className = 'infoJS';
   list.appendChild(listItem1);
 
   let listItem2 = document.createElement('li');
+  listItem2.className = 'infoJS';
   list.appendChild(listItem2);
 
   let listItem3 = document.createElement('li');
+  listItem3.className = 'infoJS';
   list.appendChild(listItem3);
 
   let listItem4 = document.createElement('li');
+  listItem4.className = 'infoJS';
   list.appendChild(listItem4);
 
   let listItem5 = document.createElement('li');
+  listItem5.className = 'infoJS';
   list.appendChild(listItem5);
 
   let listItem6 = document.createElement('li');
+  listItem6.className = 'infoJS';
   list.appendChild(listItem6);
 
   image.src = userHoroscopes[i].image;
